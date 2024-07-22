42da1bcf5a9477809e3122f184cb813cca946e93
Beam <b3bb0z@gmail.com>
2024-07-22 20:08:02 +0700

    yup

--- readme.md
+++ readme.md
@@ -3,11 +3,11 @@
 
 ### ป้อนข้อมูลนักศึกษา
 
-รหัส นศ.:
+รหัส นศ.:660610798
 
-ชื่อ-สกุล :
+ชื่อ-สกุล :ศุภกร บูรณะชาติ
 
-GitHub Page URL :
+GitHub Page URL :https://cpe207-students-167.github.io/lab07-bbeamuu/
 
 ---
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,13 @@
 // create reference for input fields.
 const firstNameInput = document.querySelector("#first-name-input");
 const lastNameInput = document.querySelector("#last-name-input");
+const emailInput = document.querySelector("#email-input");
+const passwordInput = document.querySelector("#password-input");
+const confirmPasswordInput = document.querySelector("#password-confirm-input");
 
 // create reference for buttons.
 const submitBtn = document.querySelector("#submit-btn");
+const resetBtn = document.querySelector("#reset-btn");
 
 // simple email validation
 function validateEmail(email) {
@@ -21,27 +25,98 @@ firstNameInput.onkeyup = () => {
 // add callback functions for other input events.
 // (lastname, email, password, confirm password)
 
+lastNameInput.onkeyup = () => {
+  lastNameInput.classList.remove("is-valid");
+  lastNameInput.classList.remove("is-invalid");
+};
+emailInput.onkeyup = () => {
+  emailInput.classList.remove("is-valid");
+  emailInput.classList.remove("is-invalid");
+};
+passwordInput.onkeyup = () => {
+  passwordInput.classList.remove("is-valid");
+  passwordInput.classList.remove("is-invalid");
+};
+confirmPasswordInput.onkeyup = () => {
+  confirmPasswordInput.classList.remove("is-valid");
+  confirmPasswordInput.classList.remove("is-invalid");
+};
+
 // add callback function for submit button.
 submitBtn.onclick = () => {
   isFirstNameOk = false;
+  isLastNameOk = false;
+  emailOk = false;
+  passOk = false;
+  confirmPassOk = false;
 
   // validate first name
-  if (firstNameInput.value !== "CPE207") {
-    firstNameInput.classList.add("is-invalid");
-  } else {
+  if (firstNameInput.value != "") {
     firstNameInput.classList.add("is-valid");
     isFirstNameOk = true;
+  } else {
+    firstNameInput.classList.add("is-invalid");
   }
 
   // validate last name
+  if (lastNameInput.value != "") {
+    lastNameInput.classList.add("is-valid");
+    isLastNameOk = true;
+  } else {
+    lastNameInput.classList.add("is-invalid");
+  }
 
   // validate email
+  if (validateEmail(emailInput.value)) {
+    emailInput.classList.add("is-valid");
+    emailOk = true;
+  } else {
+    emailInput.classList.add("is-invalid");
+  }
 
   // validate password
+  if (passwordInput.value.length >= 6) {
+    passwordInput.classList.add("is-valid");
+    passOk = true;
+  } else {
+    passwordInput.classList.add("is-invalid");
+  }
 
   // validate confirm password
+  if (confirmPasswordInput.value == passwordInput.value && confirmPasswordInput.value != "") {
+    confirmPasswordInput.classList.add("is-valid");
+    confirmPassOk = true;
+  } else {
+    confirmPasswordInput.classList.add("is-invalid");
+  }
 
-  if (isFirstNameOk) alert("Registered successfully");
+  if (isFirstNameOk && isLastNameOk && emailOk && passOk && confirmPassOk) alert("Registered successfully");
 };
 
 // add callback function for Reset button.
+
+resetBtn.onclick = () => {
+  isFirstNameOk = false;
+  isLastNameOk = false;
+  emailOk = false;
+  passOk = false;
+  confirmPassOk = false;
+
+  firstNameInput.value = "";
+  firstNameInput.classList.remove("is-valid");
+  firstNameInput.classList.remove("is-invalid");
+  lastNameInput.value = "";
+  lastNameInput.classList.remove("is-valid");
+  lastNameInput.classList.remove("is-invalid");
+  emailInput.value = "";
+  emailInput.classList.remove("is-valid");
+  emailInput.classList.remove("is-invalid");
+  passwordInput.value = "";
+  passwordInput.classList.remove("is-valid");
+  passwordInput.classList.remove("is-invalid");
+  confirmPasswordInput.value = "";
+  confirmPasswordInput.classList.remove("is-valid");
+  confirmPasswordInput.classList.remove("is-invalid");
+};
+
+// supakorn booranachart 660610798
